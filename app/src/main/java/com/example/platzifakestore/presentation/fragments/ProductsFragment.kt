ad97dbd85d45bc75861dbd4722ec870772e20b03
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 02:22:32 -0500

    Init UI for products recycler view

--- app/src/main/java/com/example/platzifakestore/presentation/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/platzifakestore/presentation/fragments/ProductsFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.platzifakestore.databinding.FragmentProductsBinding
+import com.example.platzifakestore.presentation.adapters.ProductAdapter
 
 class ProductsFragment : Fragment(), BaseFragment {
 
     private lateinit var binding: FragmentProductsBinding
+    private val adapter = ProductAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProductsBinding.inflate(layoutInflater, container, false)
+
+        bindViewModel()
+        initObservers()
+        initUI()
+        loadData()
 
         return binding.root
     }
@@ -29,7 +37,9 @@ class ProductsFragment : Fragment(), BaseFragment {
     }
 
     override fun initUI() {
-        // Empty
+        binding.rvProductsList.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.rvProductsList.adapter = adapter
     }
 
     override fun loadData() {
