d23bb0d79c9bbae3241fa2dcc54ddd09a8b4add5
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 02:32:55 -0500

    Created view model for products

---
+++ app/src/main/java/com/example/platzifakestore/presentation/viewmodels/ProductsViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.platzifakestore.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.platzifakestore.domain.models.Category
+import com.example.platzifakestore.domain.models.Product
+
+class ProductsViewModel : ViewModel() {
+    fun getAllProducts(): List<Product> = listOf(
+        Product(
+            id = 4,
+            title = "Handmade Fresh Table",
+            price = 687,
+            description = "Andy shoes are designed to keeping in...",
+            category = Category(
+                id = 5,
+                name = "Others",
+                image = "https://placeimg.com/640/480/any?r=0.591926261873231"
+            ),
+            images = listOf(
+                "https://placeimg.com/640/480/any?r=0.9178516507833767",
+                "https://placeimg.com/640/480/any?r=0.9300320592588625",
+                "https://placeimg.com/640/480/any?r=0.8807778235430017"
+            ),
+        )
+    )
+}
