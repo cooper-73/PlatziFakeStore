c4b863e90056ae3435e884a17419804fda19330a
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 12:48:30 -0500

    Renamed products service

--- app/src/main/java/com/example/platzifakestore/data/datasources/network/services/ProductsService.kt
+++ app/src/main/java/com/example/platzifakestore/data/datasources/network/services/ProductsService.kt
@@ -3,7 +3,7 @@ package com.example.platzifakestore.data.datasources.network.services
 import com.example.platzifakestore.data.datasources.network.models.ProductModel
 import retrofit2.http.GET
 
-interface ProductService {
+interface ProductsService {
     @GET("v1/products")
     suspend fun getAllProducts(): List<ProductModel>
 }
