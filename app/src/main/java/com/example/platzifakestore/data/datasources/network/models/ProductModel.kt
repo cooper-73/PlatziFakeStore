fb81f048069df9af29ae21d431d86016b3d32dee
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 12:29:42 -0500

    Added serialized names to product model

--- app/src/main/java/com/example/platzifakestore/data/datasources/network/models/ProductModel.kt
+++ app/src/main/java/com/example/platzifakestore/data/datasources/network/models/ProductModel.kt
@@ -1,16 +1,27 @@
 package com.example.platzifakestore.data.datasources.network.models
 
+import com.google.gson.annotations.SerializedName
+
 data class ProductModel(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("price")
     val price: Int,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("category")
     val category: CategoryModel,
+    @SerializedName("images")
     val images: List<String>
 )
 
 data class CategoryModel(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("image")
     val image: String,
 )
