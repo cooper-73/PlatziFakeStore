44017ffaa23f769e8f410252d8da4331b59d9306
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 13:24:51 -0500

    Added live data for list of products

--- app/src/main/java/com/example/platzifakestore/presentation/viewmodels/ProductsViewModel.kt
+++ app/src/main/java/com/example/platzifakestore/presentation/viewmodels/ProductsViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.platzifakestore.presentation.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.platzifakestore.domain.models.Category
 import com.example.platzifakestore.domain.models.Product
 
 class ProductsViewModel : ViewModel() {
+
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> = _products
+
     fun getAllProducts(): List<Product> = listOf(
         Product(
             id = 4,
@@ -20,7 +26,7 @@ class ProductsViewModel : ViewModel() {
                 "https://placeimg.com/640/480/any?r=0.9178516507833767",
                 "https://placeimg.com/640/480/any?r=0.9300320592588625",
                 "https://placeimg.com/640/480/any?r=0.8807778235430017"
-            ),
+            )
         )
     )
 }
