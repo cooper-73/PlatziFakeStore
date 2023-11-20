528ec3bac1e8b6a166c1e30dd2bbb77876e81ba9
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-20 04:02:36 -0500

    Added dao method to sort products by price

--- app/src/main/java/com/example/platzifakestore/data/datasources/database/daos/ProductsDao.kt
+++ app/src/main/java/com/example/platzifakestore/data/datasources/database/daos/ProductsDao.kt
@@ -35,4 +35,13 @@ interface ProductsDao {
     )
     suspend fun filterProductsByTitle(title: String): List<ProductWithCategory>
 
+    @Transaction
+    @Query(
+        "SELECT products.category_id AS categoryId, products.*, categories.name AS categoryName, categories.image AS categoryImage " +
+                "FROM products INNER JOIN categories ON products.category_id = categories.id ORDER BY " +
+                "CASE WHEN :isAsc = 1 THEN products.price END ASC, " +
+                "CASE WHEN :isAsc = 0 THEN products.price END DESC"
+    )
+    suspend fun getProductsSortedByPrice(isAsc: Boolean): List<ProductWithCategory>
+
 }
