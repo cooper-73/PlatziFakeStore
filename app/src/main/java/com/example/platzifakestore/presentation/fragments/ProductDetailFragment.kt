d2f8c0b5aa22546892a60e3fe3a7e12acf60ef65
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 22:09:03 -0500

    Implemented base fragment in product detail fragment

--- app/src/main/java/com/example/platzifakestore/presentation/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/example/platzifakestore/presentation/fragments/ProductDetailFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.platzifakestore.R
+import androidx.fragment.app.viewModels
+import com.example.platzifakestore.databinding.FragmentProductDetailBinding
+import com.example.platzifakestore.presentation.viewmodels.ProductsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class ProductDetailFragment : Fragment() {
+@AndroidEntryPoint
+class ProductDetailFragment : Fragment(), BaseFragment {
+
+    private lateinit var binding: FragmentProductDetailBinding
+    private val viewModel: ProductsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    ): View {
+        binding = FragmentProductDetailBinding.inflate(layoutInflater, container, false)
+
+        initObservers()
+        initUI()
+        loadData()
+
+        return binding.root
+    }
+
+    override fun initObservers() {
+        // Empty
+    }
+
+    override fun initUI() {
+        // Empty
+    }
+
+    override fun loadData() {
+        // Empty
     }
 
 }
