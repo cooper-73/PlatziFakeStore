e296325770db4bd8712cad078fffce6d92beb131
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-19 22:16:41 -0500

    Added getProduct service

--- app/src/main/java/com/example/platzifakestore/data/datasources/network/services/ProductsService.kt
+++ app/src/main/java/com/example/platzifakestore/data/datasources/network/services/ProductsService.kt
@@ -2,8 +2,12 @@ package com.example.platzifakestore.data.datasources.network.services
 
 import com.example.platzifakestore.data.datasources.network.models.ProductModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductsService {
     @GET("v1/products")
     suspend fun getAllProducts(): List<ProductModel>
+
+    @GET("v1/products/{productId}")
+    suspend fun getProduct(@Path("productId") productId: Int): ProductModel
 }
