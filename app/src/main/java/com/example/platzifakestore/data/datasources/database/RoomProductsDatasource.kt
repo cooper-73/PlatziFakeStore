c4634132c102890d9f286e4c06f069154ffabea2
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-20 01:06:10 -0500

    Created room products datasource

---
+++ app/src/main/java/com/example/platzifakestore/data/datasources/database/RoomProductsDatasource.kt
@@ -0,0 +1,40 @@
+package com.example.platzifakestore.data.datasources.database
+
+import com.example.platzifakestore.data.datasources.ProductsDatasource
+import com.example.platzifakestore.data.datasources.database.daos.CategoriesDao
+import com.example.platzifakestore.data.datasources.database.daos.ProductsDao
+import com.example.platzifakestore.data.datasources.mappers.CategoryModelMapper
+import com.example.platzifakestore.data.datasources.mappers.ProductModelMapper
+import com.example.platzifakestore.data.datasources.network.models.ProductModel
+
+class RoomProductsDatasource(
+    private val productsDao: ProductsDao,
+    private val categoriesDao: CategoriesDao
+) : ProductsDatasource {
+    override suspend fun getAllProducts(): List<ProductModel> {
+        return productsDao.getProductsWithCategory()
+            .map { ProductModelMapper.productWithCategoryToProductModel(it) }
+    }
+
+    override suspend fun getProduct(productId: Int): ProductModel {
+        return ProductModelMapper.productWithCategoryToProductModel(
+            productsDao.getProductWithCategory(
+                productId
+            )
+        )
+    }
+
+    override suspend fun saveProducts(products: List<ProductModel>) {
+        categoriesDao.insertCategories(categories = products.map {
+            CategoryModelMapper.categoryModelToCategoryEntity(
+                it.category
+            )
+        })
+
+        productsDao.insertProducts(products = products.map {
+            ProductModelMapper.productModelToProductEntity(
+                it
+            )
+        })
+    }
+}
