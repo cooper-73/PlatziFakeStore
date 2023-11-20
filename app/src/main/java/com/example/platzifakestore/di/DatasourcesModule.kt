8a48999779da717188732bc8d3298a066dca8835
Cristhian Cruz <c.cruzcoro73@gmail.com>
2023-11-20 01:07:31 -0500

    Added named products datasource providers

--- app/src/main/java/com/example/platzifakestore/di/DatasourcesModule.kt
+++ app/src/main/java/com/example/platzifakestore/di/DatasourcesModule.kt
@@ -1,20 +1,34 @@
 package com.example.platzifakestore.di
 
 import com.example.platzifakestore.data.datasources.ProductsDatasource
+import com.example.platzifakestore.data.datasources.database.RoomProductsDatasource
+import com.example.platzifakestore.data.datasources.database.daos.CategoriesDao
+import com.example.platzifakestore.data.datasources.database.daos.ProductsDao
 import com.example.platzifakestore.data.datasources.network.RetrofitProductsDatasource
 import com.example.platzifakestore.data.datasources.network.services.ProductsService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import javax.inject.Named
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object DatasourcesModule {
 
     @Provides
+    @Named("retrofitProductsDatasource")
     fun providesRetrofitProductsDatasource(productsService: ProductsService): ProductsDatasource {
         return RetrofitProductsDatasource(productsService)
+    }
+
+    @Provides
+    @Named("roomProductsDatasource")
+    fun providesRoomProductsDatasource(
+        productsDao: ProductsDao,
+        categoriesDao: CategoriesDao
+    ): ProductsDatasource {
+        return RoomProductsDatasource(productsDao, categoriesDao)
     }
 
 }
